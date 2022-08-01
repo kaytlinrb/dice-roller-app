e7fc400d4423a7124c3b700477e8d8a6a9b10128
Katie Borel <kaytlinrb@gmail.com>
2022-08-01 12:52:17 -0700

    add images that match and display resulting dice roll

--- DiceyRoller/app/src/main/java/com/example/diceyroller/MainActivity.kt
+++ DiceyRoller/app/src/main/java/com/example/diceyroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceyroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
         class Dice(val numSides: Int) {
 
-            fun roll(): Int {
+             fun roll() : Int {
                 return (1..numSides).random()
             }
         }
@@ -24,20 +25,21 @@ class MainActivity : AppCompatActivity() {
 
 
         fun rollDice() {
-            var postText
+
             val dice = Dice(6)
             val diceRoll = dice.roll()
-            val resultTextView: TextView = findViewById(R.id.textView)
-
-            if (diceRoll != 3) {
-                postText = "You rolled a ${diceRoll}"
+            val diceImage : ImageView = findViewById(R.id.diceImg)
+            val drawableResource = when (diceRoll) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
             }
-            else {
-                postText = "Hey, You Rolled a ${diceRoll}, my lucky number!"
-            }
 
-
-            resultTextView.text = postText
+            diceImage.setImageResource(drawableResource)
+            diceImage.contentDescription = diceRoll.toString()
         }
 
 
